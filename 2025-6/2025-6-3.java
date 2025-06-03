a5b4af4f5d886fcd8504282ca0864498d01078ac
GitHub <GitHub>
2025-06-04 02:38:43 +0900

    2025-6-3

--- 2025-6/2025-6-3.java
+++ 2025-6/2025-6-3.java
@@ -10,3 +10,48 @@ public class Main {
     }
 }
 
+//　複数データを読み込む標準入力
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String data = scan.next();
+        System.out.println("hello" + data);
+
+        data = scan.next(); //2行のデータを読み込むときnextメソッドを二回呼び出す必要がある
+        System.out.println("hello" + data);
+    }
+}
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int count = scan.nextInt(); 
+        System.out.println(count);
+
+        String data;
+        for (int i = 0; i < count; i++){ //入力下整数がcount変数に代入される
+          data = scan.next();
+          System.out.println("hello" + data);
+        }
+    }
+}
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int seireki, heisei;
+        for (seireki = 1989; seireki <= 2016; seireki++){ //入力下整数がcount変数に代入される
+          System.out.print("西暦" + data + "年は");
+          heisei = seireki - 1988;
+          System.out.print("平成" + heisei + "年です");
+        }
+    }
+}
+
+//ここまで入門編３　chapter8・9の練習問題難易度あがってるからもう一回する
