c9ef3d6e12e29ae8daea70f8c22efa7ba9bf2cb0
kobakaho <kapotast@gmail.com>
2025-07-24 19:30:30 +0900

    2025-7-24

---
+++ 2025-7/2025-7-24.java
@@ -0,0 +1,33 @@
+// ソート
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        String[] sg = new String[n]; //文字列で配列を作成
+
+        for (int i = 0; i < n; i++) {
+            String g = sc.next();
+            String s = sc.next();
+
+            if (g.length() == 1) {
+                g = "0" + g;
+            }
+
+            if (s.length() == 1) {
+                s = "0" + s;
+            }
+            
+            sg[i] = s + g; //入力された数字をそれぞれ10ケタの値にして合体　i番目の配列として格納
+        }
+
+        Arrays.sort(sg, Collections.reverseOrder()); // 配列を取得し降順にソート
+
+        for (int i = 0; i < n; i++) { 
+            System.out.println(Integer.parseInt(sg[i].substring(2,4)) + " " + Integer.parseInt(sg[i].substring(0,2)));
+        } //　配列の何番目の文字を取得し整数に変換して出力
+    }
+}
